04a56d73a3e4b6cb782bd2d1bde0286273982f9a
jcarpio <jose.carpio@gmail.com>
2024-02-26 10:02:12 +0100

    Update factorial.pl

--- factorial.pl
+++ factorial.pl
@@ -1,3 +1,22 @@
 
+% Factorial
 
-% factorial
+/*
+comentario de bloque
+*/
+
+% Principio de inducción matemático
+
+% factorial(+N, -R)
+%  es cierto si R unifica con un número que representa
+%  el factorial de N 
+
+factorial(1, 1).
+
+% factorial(N) es N * factorial(N-1)
+
+% una variable una vez que toma un valor, nunca cambia 
+% hasta que termina su ámbito. 
+
+factorial(N, R2) :- N > 1, N2 is N - 1, factorial(N2, R), R2 is R * N.
+
