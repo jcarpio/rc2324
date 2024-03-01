e1bc0f27233c4924e497715c6d9be016fa7fb086
jcarpio <jose.carpio@gmail.com>
2024-03-01 10:09:22 +0100

    Update natural.pl

--- natural.pl
+++ natural.pl
@@ -7,9 +7,7 @@
 % 1) P es cierta para n0
 
 % 2) n > n0, Si P(n-1) -> P(n)
-
-             natural(N) :- N > 1, N2 is N - 1, natural(N2). 
-
+        
 natural(1).
 
 natural(N) :- N > 1, N2 is N - 1, natural(N2).
