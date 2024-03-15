6640e1ed269b0ac522cccf9384d103e5def53bb4
jcarpio <jose.carpio@gmail.com>
2024-03-15 11:34:17 +0100

    Update burbuja.pl

--- burbuja.pl
+++ burbuja.pl
@@ -1,14 +1,20 @@
-/*
 
-burbuja(+Lista, -R)
+% burbuja(+Lista, -R)
 
-ordenada(+Lista)
+% ordenada(+Lista)
 
 ordenada([]).
+
+ordenada([_]).
 
 ordenada([Cab1, Cab2|Resto]):- 
   Cab1 < Cab2,
   ordenada([Cab2|Resto]).
 
+% burbuja([1,5,3,4], ).
 
-*/
+burbuja(Lista, Lista):- ordenada(Lista).
+
+burbuja(Lista, R):- append(L1, [E1,E2|L2], Lista), E1 > E2,
+                    append(L1, [E2, E1|L2], Temp),
+				    burbuja(Temp, R).
