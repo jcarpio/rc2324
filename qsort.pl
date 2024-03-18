3fe62801578bb529d725a31646623ca1c87b815f
jcarpio <jose.carpio@gmail.com>
2024-03-18 09:48:23 +0100

    Create qsort.pl

---
+++ qsort.pl
@@ -0,0 +1,21 @@
+/*
+
+qsort(+Lista, -R)
+  es cierto si R unifica con una lista que contiene
+  los elementos de Lista ordenados de menor a mayor.
+ 
+
+
+*/
+
+
+/*
+
+divide(+Pivote, +Lista, -Menores, -Mayores)
+  es cierto si Menores unifica con una lista que 
+  contiene los elementos de Lista que son menores
+  o iguales que el Pivote y Mayores unifica
+  con una lista que contiene los elementos de 
+  Lista que son Mayores que el Pivote.
+
+*/
