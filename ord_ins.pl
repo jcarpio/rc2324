6c51bcdd50735ada61c9def47370a4a8cecca0e4
jcarpio <jose.carpio@gmail.com>
2024-03-18 09:46:54 +0100

    Update ord_ins.pl

--- ord_ins.pl
+++ ord_ins.pl
@@ -1,9 +1,37 @@
-
-
 /*
 
-insercion(+Lista, -R)
+ord_ins(+Lista, -R)
   es cierto si R unifica con una lista que contiene
   los elementos de Lista ordenados de menor a mayor.
   
 */
+
+ord_ins([], []).
+
+ord_ins([Cab|Resto], R2):-  ord_ins(Resto, R), 
+   ins_en_ord(Cab, R, R2).
+   
+   
+/*
+ins_en_ord(+Elem, +Lista, -R)
+  es cierto si R unifica con una lista que
+  contiene los elementos de Lista (que es una 
+  lista ordenada de menor a mayor) con
+  Elem insertado en su posición correcta.
+
+*/
+
+ins_en_ord(Elem, [], [Elem]).
+
+ins_en_ord(Elem, [Cab|Resto], [Elem, Cab|Resto]):-
+   Elem =< Cab.
+   
+ins_en_ord(Elem, [Cab|Resto], [Cab|R]):-
+   Elem > Cab, ins_en_ord(Elem, Resto, R).
+   
+
+
+
+
+
+
