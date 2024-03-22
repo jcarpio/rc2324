57312c68b1d481633457945b5719b5dac1ad2d44
jcarpio <jose.carpio@gmail.com>
2024-03-22 13:25:17 +0100

    Update primos_xy.pl

--- primos_xy.pl
+++ primos_xy.pl
@@ -10,14 +10,21 @@
 
 
 /*
-  lista_divisores(+N, +Y, -Lista)
+  lista_divisores(+N, +Rango, -Lista)
     es cierto si Lista unifica con los divisores de N que
-	van desde 1 hasta Y. Y es un número entre 1 y N
+	van desde 1 hasta Rango [1,Rango]. Y es un número entre 1 y N.
+	
+  lista_divisores(8, 8, R).
+  R=[1, 2, 4, 8]
 	
 */
 
 lista_divisores(_, 1, [1]).
 
-lista_divisores(N, Y, [Y|R]):- Y2 is Y-1, 0 is mod(N,Y), lista_divisores(N, Y2, R).
-
-lista_divisores(N, Y, R):- Y2 is Y-1, M is mod(N,Y), M \= 0, lista_divisores(N, Y2, R).
+lista_divisores(N, Rango, [Rango|R]):- Rango > 1, Rango2 is Rango - 1,
+   0 is mod(N,Rango), 
+   lista_divisores(N, Rango2, R).
+   
+lista_divisores(N, Rango, R):- Rango > 1, Rango2 is Rango - 1,
+   Mod is mod(N,Rango), Mod \= 0,
+   lista_divisores(N, Rango2, R).
