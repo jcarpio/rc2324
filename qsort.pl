b49e977ee677a28c58764a648bdbe1c83082a212
jcarpio <jose.carpio@gmail.com>
2024-03-22 10:58:24 +0100

    Update qsort.pl

--- qsort.pl
+++ qsort.pl
@@ -1,13 +1,22 @@
+:- set_prolog_stack(global, limit(16 000 000 000)).
+:- set_prolog_stack(trail,  limit(16 000 000 000)).
+:- set_prolog_stack(local,  limit(16 000 000 000)).
+
+
 /*
 
 qsort(+Lista, -R)
   es cierto si R unifica con una lista que contiene
   los elementos de Lista ordenados de menor a mayor.
  
-
-
 */
 
+qsort([], []).
+qsort([Pivote|Resto], R):-
+   divide(Pivote, Resto, Me, Ma),
+   qsort(Me, RMe), 
+   qsort(Ma, RMa), 
+   append(RMe, [Pivote|RMa], R).
 
 /*
 
@@ -19,3 +28,11 @@ divide(+Pivote, +Lista, -Menores, -Mayores)
   Lista que son Mayores que el Pivote.
 
 */
+
+divide(_, [], [], []).
+
+divide(Pivote, [Cab|Resto], [Cab|RMe], RMa):- Cab =< Pivote,
+  divide(Pivote, Resto, RMe, RMa).
+  
+divide(Pivote, [Cab|Resto], RMe, [Cab|RMa]):- Cab > Pivote,
+  divide(Pivote, Resto, RMe, RMa).  
