65549db0d9ffe00215fed222f3c80a73303261ca
jcarpio <jose.carpio@gmail.com>
2024-04-19 11:23:01 +0200

    Create anchura_ag.pl

---
+++ arboles_genericos/anchura_ag.pl
@@ -0,0 +1,9 @@
+/*
+anchura(+ArbolGenerico, -ListaAnchura)
+  es cierto si ListaAnchura unifica con la lista de etiquetas del recorrido
+  en anchura de ArbolGenerico.
+  
+*/
+
+arbol( a(1, [a(2, [a(5,[]), a(6,[]), a(7,[])]), a(3,[a(8,[])]), a(4, [a(9, []), a(10,[]), a(11,[])])]) ).
+  
