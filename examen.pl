a047fab1cbb2cb26b3e92b0b1daae83e1a5cd28f
jcarpio <jose.carpio@gmail.com>
2024-06-07 11:41:13 +0200

    Update examen.pl

--- examen.pl
+++ examen.pl
@@ -30,6 +30,15 @@ CLP
 10. Sudoku
 11. Planificador de horarios
 
+reverse(+Lista, -R)
+
+*/
+
+reverse([], []).
+reverse(Lista, R):- append([L1, [E]], Lista), reverse(L1, L1R), 
+   append([E], L1R, R).   
+
+/*
 
 Eimina cada N
 
@@ -81,3 +90,51 @@ mayor([], _, 0).
 mayor([(E,N)|R], E, N):-  mayor(R, _, N2), N >= N2.
    
 mayor([(_,N)|R], E2, N2):-  mayor(R, E2, N2), N < N2. 
+
+
+
+/*
+
+lista_hojas(+ArbolGenerico, -R )
+  es cierto si R unifica con una lista que contiene
+  las hojas de ArbolGenerico.
+
+*/
+
+lista_hojas(a(Et, []), [Et]).
+lista_hojas(a(_, ListaHijos), R):- ListaHijos \= [], lista_hojas(ListaHijos, R).
+
+lista_hojas([], []).
+lista_hojas([Cab|Resto], R):-  lista_hojas(Resto, RResto), lista_hojas(Cab, RCa),
+  append([RCa, RResto], R).
+  
+arbol_1(  a(1, [a(2, [a(3,[]), a(4,[]), a(5,[])]), a(6, []), a(7, [a(8,[]), a(9,[])])])  ).   
+
+/*
+
+balanceado(+ArbolBinario)
+  es cierto si Arbolbinario es balanceado. Un árbol es balanceado
+  si para cada nodo del árobol, la diferencia entre la altura
+  del Hijo Izquierda y el Hijo Derecha es como máximo 1 en valor
+  absoluto.
+
+*/
+
+balanceado(a(_, HI, HD),  ):-
+  altura(HI, AI),
+  altura(HD, AD),
+  X is abs(AI - AD), X =< 1,
+  balanceado(HI),
+  balanceado(HD).
+  
+altura(nil, 0).
+altura(a(_, HI, HD), R):- altura(HI, AI), altura(HD, AD), A is max(AI, AD), R is A + 1.
+  
+
+/*
+
+anchura(+ArbolBinario, -R) 
+  es cierto si R unifica con una lista con las etiquetas 
+  de ArbolBinario recorridas en anchura.  
+
+*/
